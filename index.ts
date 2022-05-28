4a0b57363817dc56f30c4f52e557e3c53ee72552
Gracjan Jaworski <grajaworski@grajaworski.pl>
2022-05-28 16:41:03 +0200

    Added error handling.

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
-import express, { json } from "express";
+import express, {json} from "express";
 import cors from "cors";
+import {handleError, ValidationError} from "./utils/erorrs";
 import 'express-async-errors';
 
 const app = express();
@@ -9,6 +10,13 @@ app.use(cors({
 }));
 app.use(json());
 
+// Routes...
+
+// app.get('/', async (req, res) => {
+//     throw new ValidationError('Daamn!');
+// });
+
+app.use(handleError);
 
 app.listen(3001, '0.0.0.0', () => {
     console.log('Listening on port http://localhost:3001');
